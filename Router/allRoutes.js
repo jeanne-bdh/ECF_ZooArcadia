975f08c91fa42ec6c38bc2884a1281093d810634
eurajane <jbouchend@gmail.com>
2024-09-25 05:11:33 +0200

    ajout page contact

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -6,6 +6,7 @@ export const allRoutes = [
     new Route("/services", "Nos services", "/pages/services.html"),
     new Route("/habitats", "Nos habitats", "/pages/habitats.html"),
     new Route("/signin", "Connexion", "/pages/signin.html"),
+    new Route("/contact", "Contact", "/pages/contact.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
