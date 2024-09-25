9cadbacde1f5127ce3f8f596c7f6c803b5264745
eurajane <jbouchend@gmail.com>
2024-09-25 06:27:05 +0200

    maj page hoome section avis

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,8 +5,9 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/services", "Nos services", "/pages/services.html"),
     new Route("/habitats", "Nos habitats", "/pages/habitats.html"),
-    new Route("/signin", "Connexion", "/pages/signin.html"),
     new Route("/contact", "Contact", "/pages/contact.html"),
+    new Route("/signin", "Connexion", "/pages/signin.html"),
+    new Route("/formAvis", "Formulaire Avis", "/pages/formAvis.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
