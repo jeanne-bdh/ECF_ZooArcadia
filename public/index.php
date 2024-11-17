62d134b9152e65e0697cc2e5a062a4a1152cf1a3
eurajane <jbouchend@gmail.com>
2024-11-17 20:33:02 +0100

    update deploiement

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . "/pages/header.php";
+require_once __DIR__ . "/../pages/header.php";
 
 ?>
 
@@ -10,6 +10,6 @@ require_once __DIR__ . "/pages/header.php";
 
 <?php
 
-require_once __DIR__ . "/pages/footer.php";
+require_once __DIR__ . "/../pages/footer.php";
 
 ?>
